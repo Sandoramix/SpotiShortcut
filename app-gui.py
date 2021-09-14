19c9369d04004c0042c4e7fa00be6a1209bd8992
Sandoramix <sandoramixer61@gmail.com>
2021-09-14 15:51:55 +0200

    v0.02

--- app-gui.py
+++ app-gui.py
@@ -1,4 +1,4 @@
-from logging import setLoggerClass
+from logging import debug, setLoggerClass
 import subprocess
 
 import datetime
@@ -14,7 +14,7 @@ from requests.sessions import session
 import spotipy as spoti
 from spotipy.oauth2 import SpotifyOAuth
 import json
-
+ 
 import psutil
 
 import tkinter as tk
@@ -33,7 +33,14 @@ class Spoti():
 
         
         with open('.cache') as t:
-            self.TOKEN=json.load(t)['refresh_token']
+            self.TOKEN=json.load(t)['access_token']
+        
+
+        #print(self.session.che)
+        #spotify:track:1VvwxqIfKRazmvE7zdDFRg
+
+        
+
         #print(self.TOKEN)
         #self.add_item_to_playlist(songs=['spotify:track:0uTEaevJy8TUD8MgzuEOc5'])
         
@@ -59,10 +66,23 @@ class Spoti():
         #self.remove_item_from_playlist(songs=['3Dzso9Q2WwupEclqgxBZht'])
         #self.song_names_from_playlist() 
 
+
+
+
+
+    def items_in_saved_check(self,songs=[]):
+        return list(req.get('https://api.spotify.com/v1/me/tracks/contains?ids='+','.join(songs),headers={'Authorization':f'Bearer {self.TOKEN}'}).json())
+
+
     def current_song(self):
         return self.session.current_playback()
+
+
     def current_song_id(self) -> str:
-        return self.current_song()['item']['id']
+        try:
+            return self.current_song()['item']['id']
+        except:
+            print('NO SONG IS PLAYING')
 
 
     def song_name(self,song=None) -> str:
@@ -70,54 +90,59 @@ class Spoti():
             song=self.current_song()['item']
         elif isinstance(song,str):
             song=self.session.track(song)
-
-
         artists=''
         for i in song['artists']:
             artists+=i['name']+", "
         artists=artists[:len(artists)-2]
-        
+
         return song['name'] + ' - '+ artists
     
+
     def song_ids_from_playlist(self,playlist='0c0dqQKnUr2O6nENHG3Lez'):
         raw=self.session.playlist(playlist_id=playlist,fields='tracks.items(track(id))')
         ids=[]
         for i in raw['tracks']['items']:
-
-            # name=i['track']['name']+' - '
-            # for j in i['track']['artists']:
-            #     name+=j['name']+', '
-            # name=name[:len(name)-2]
             ids.append(i['track']['id'])
+
         return ids
 
 
+    def add_items_to_liked(self,songs=[]):
+        check=self.items_in_saved_check(songs)
 
+        for i in range(len(songs)):
+            if check[i]==False:
+                self.session.current_user_saved_tracks_add([songs[i]])
+                print(self.song_name(songs[i]) + " - ADDED TO LIKED SONGS")
+            else:
+                print(self.song_name(songs[i]) + " - IS ALREADY IN LIKED SONGS")
 
 
     def add_current_to_playlist(self,playlist='0c0dqQKnUr2O6nENHG3Lez'):
-        self.add_item_to_playlist(songs=[self.current_song_id()],playlist=playlist)
+        self.add_items_to_playlist(songs=[self.current_song_id()],playlist=playlist)
 
     def remove_current_from_playlist(self,playlist='0c0dqQKnUr2O6nENHG3Lez'):
-        self.remove_item_from_playlist(songs=[self.current_song_id()],playlist=playlist)
+        self.remove_items_from_playlist(songs=[self.current_song_id()],playlist=playlist)
+    
+    def add_current_to_liked(self):
+        self.add_items_to_liked(songs=[self.current_song_id()])
 
 
 
-
-    def add_item_to_playlist(self,songs,playlist='0c0dqQKnUr2O6nENHG3Lez'):
+    def add_items_to_playlist(self,songs,playlist='0c0dqQKnUr2O6nENHG3Lez'):
         pl_songs=self.song_ids_from_playlist(playlist=playlist)
         for i in songs:
             if i not in pl_songs:
                 self.session.playlist_add_items(playlist,songs)
-                print(self.song_name(song=i)+' ADDED FROM PLAYLIST')
+                print(self.song_name(song=i)+' ADDED TO PLAYLIST')
             else:
                 print(self.song_name(song=i)+' IS ALREADY IN PLAYLIST')
 
 
-    def remove_item_from_playlist(self,songs=[],playlist='0c0dqQKnUr2O6nENHG3Lez'):
+    def remove_items_from_playlist(self,songs=[],playlist='0c0dqQKnUr2O6nENHG3Lez'):
         pl_songs=self.song_ids_from_playlist(playlist=playlist)
         for i in songs:
-            if i not in pl_songs:
+            if i in pl_songs:
                 self.session.playlist_remove_all_occurrences_of_items(playlist,songs)
                 print(self.song_name(song=self.session.track(i))+' REMOVED FROM PLAYLIST')
             else:
@@ -139,54 +164,12 @@ def press(key):
         k = key.name
     if k =="f7":
         spotify.remove_current_from_playlist(playlist='0XEZPioQQwZfuiTpVAGNlp')
-    elif k == '+' or key=='f8':
+    elif k == '+':
+        spotify.add_current_to_liked()
+    elif k=='f8':
         spotify.add_current_to_playlist(playlist='0XEZPioQQwZfuiTpVAGNlp')
     elif k =='f9':
         exit()
-
-
-# class App(tk.Frame):
-#     global spotify
-#     def __init__(self,master=None):
-
-        
-#         #-----------------------
-#         super().__init__(master)
-#         self.master=master
-#         master.geometry('400x300')
-#         master.resizable(0, 0)
-#         master.option_add('*Background','black')
-#         master.option_add("*Button.Background",'black')
-#         master.option_add("*Button.Foreground", "green")
-        
-#         self.pack()
-#         self.create_widgets()
-        
-#         master.mainloop()
-        
-
-#     def create_widgets(self):
-#         self.bottom=tk.Frame(self.master,height=150,width=400)
-#         self.top=tk.Frame(self.master,height=150,width=400)
-#         self.bottom_left=tk.Frame(self.bottom,height=150,width=200)
-#         self.add_song=tk.Button(self.bottom_left,text='Add song',height=6,width=30,bd=1,font=10,command=spotify.add_current_to_playlist)
-#         self.rem_song=tk.Button(self.bottom_left,text='Remove song',height=5,width=30,bd=1,font=10,command=spotify.remove_current_from_playlist)
-        
-#         self.quit=tk.Button(self.bottom, text="Quit", fg="red",height=12,width=30,bd=1,font=10,command=self.master.destroy)#exit
-
-
-#         self.bottom.pack(side=tk.BOTTOM,fill=tk.BOTH)
-#         self.bottom_left.pack(side=tk.LEFT)
-        
-#         self.view_list.pack(side=tk.LEFT)
-
-#         self.quit.pack(side=tk.RIGHT)
-#         self.add_song.pack(side=tk.TOP)
-        
-#         self.rem_song.pack(side=tk.BOTTOM)
-        
-#         self.top.pack(side=tk.TOP)
-# app=App(master=tk.Tk(className=' Spotify List Updater'))
 
 
 
@@ -194,20 +177,39 @@ def press(key):
 def process_exists(processName):
     return processName in (p.name() for p in psutil.process_iter())
 
+if(not process_exists("Spotify.exe")):
+    subprocess.run(r"C:\Users\sando\AppData\Roaming\Spotify\Spotify.exe")  
  
 with keyboard.Listener(on_press=press) as listener:
     listener.join()
 
-if(not process_exists("Spotify.exe")):
-    subprocess.run(r"C:\Users\sando\AppData\Roaming\Spotify\Spotify.exe")  
-
-si=subprocess.STARTUPINFO()
-si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
 
 
 
+        #print(self.TOKEN)
+        #self.add_item_to_playlist(songs=['spotify:track:0uTEaevJy8TUD8MgzuEOc5'])
+        
+        # self.body={'grant_type':'client_credentials','scopes': self.SCOPES,}
+        # self.response=req.post('https://accounts.spotify.com/api/token',data=self.body,auth=(self.ID,self.SECRET))
+        # self.TOKEN=self.response.json()['access_token']
 
+        # self.bd={
+        #     'Content-Type':'application/json',
+        #     'Authorization':'Bearer '+self.TOKEN,
+        # }
+        # self.response=req.get('https://api.spotify.com/v1/playlists/0c0dqQKnUr2O6nENHG3Lez?fields=tracks.items(track(name%2Cartists.name))',headers=self.bd)
+        # print(self.response.json())
 
+        #self.song_ids_from_playlist()
+        #print(self.response.content)
+        #print(self.TOKEN)
+        #get cookie info for start_session(sp_dc, sp_key)
+        #https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2F
+        #self.session=spotify_token.start_session("AQBJ2V_6dHFWJJ30ZDzXTkk6O8rqlPdDRX3KAR8pQpucSOUFmVICWjvbr8Uk8h6fORhJ-iJ3EYoAMZvoAJIiycD7NCW5Js1OLdZVAh926KmEdQ","6f6ec96d-685c-41e6-8d51-65f9fb0867ca")
+        #self.TOKEN=self.session[0]
+        #print(self.session.playlist_tracks(playlist_id='0XEZPioQQwZfuiTpVAGNlp',fields='itemss'))
+        #self.remove_item_from_playlist(songs=['3Dzso9Q2WwupEclqgxBZht'])
+        #self.song_names_from_playlist() 
 
 
