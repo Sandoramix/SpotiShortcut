ef556584538a18690f75445fb101a5ffa75dfb51
Sandoramix <sandoramixer61@gmail.com>
2021-10-23 23:31:48 +0200

    bat script's syntax fixed

---
+++ init.bat
@@ -0,0 +1,5 @@
+pip install pipenv
+pipenv run pip install -r requirements.txt
+echo cls > start.bat
+echo pipenv run main.py >> start.bat
+start.bat
--- init.cmd
+++
@@ -1,5 +0,0 @@
-python install pipenv
-pipenv run pip install -r requirements.txt
-echo cls > start.cmd
-echo pipenv run main.py >> start.cmd
-start.cmd
