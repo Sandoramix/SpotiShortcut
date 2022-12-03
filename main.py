24c02d7e683fc913ba183b779bd52d6393be373e
sandoramix <sandoramixer61@gmail.com>
2022-12-03 22:32:52 +0100

    implemented new logic

--- main.py
+++ main.py
@@ -1,86 +1,102 @@
+import os
 from pynput import keyboard
 import threading
 from time import sleep
-import Spotify_custom as s
+import app.Spotify_custom as s
+from app.utils import *
+
+
+CONFIG_PATH=os.path.abspath("./config.yaml")
+
+CONFIG=loadConfig(CONFIG_PATH)
+
 
 from dotenv import load_dotenv
 load_dotenv()
 
 pause = False
+SPOTIFY = s.Spotify_custom()
 
+
+SHORTCUTS={}
+
+def populateShortcuts(name,command,multiple=False):
+	global SHORTCUTS
+	if not name in CONFIG: return None
+	data = CONFIG[name]
+	if not data: return None
+	if multiple and (type(data) is not dict): return None
+	if not multiple and (type(data) is not str): return None
+
+	if not multiple:
+		SHORTCUTS[data]=[command,None]
+		return
+	
+
+	for key,value in data.items():
+		if not value or type(value) is not str: continue
+		SHORTCUTS[key]=[command,value]
+	
+
+def updateShortcuts():
+	global SHORTCUTS
+	SHORTCUTS={}
+	populateShortcuts(ADD_TO_PLAYLIST,SPOTIFY.add_current_to_playlist,True)
+	populateShortcuts(REM_FR_PLAYLIST,SPOTIFY.remove_current_from_playlist,True)
+	
+	populateShortcuts(ADD_TO_LIKED,SPOTIFY.add_current_to_liked)
+	populateShortcuts(REM_FR_LIKED,SPOTIFY.remove_current_from_liked)
+	
+	populateShortcuts(TGL_LOOP,SPOTIFY.loop_toggle)
+	populateShortcuts(TGL_SHUFFLE,SPOTIFY.shuffle_toggle)
+	populateShortcuts(PAUSE,None)
+
+	populateShortcuts(UPD_SHORTCUTS,updateShortcuts)
+	print(line("-"))
+
+	
+updateShortcuts()
 
 def press(key):
-    global pause
-    #
-    # FULLY CUSTOMIZABLE SHORCUTS
-    # "Key" : [action,parameter] ->Used for [add_current_to_playlist,remove_current_from_playlist] commands
-    # "Key" : [action,None] ->Used for [ add_current_to_liked,remove_current_from_liked,loop_toggle,shuffle_toggle] commands
-
-    # Spotify available actions:[
-    #   add_current_to_playlist , remove_current_from_playlist
-    #   add_current_to_liked , remove_current_from_liked
-    #   loop_toggle , shuffle_toggle
-    # ]
-    shorcuts = {
-        "f10": [close, None],
+	global pause
+	if not SHORTCUTS:
+		updateShortcuts()
 
 
-        "f6": [spotify.remove_current_from_playlist, 'PLAYLIST'],
-        "f7": [spotify.add_current_to_playlist, 'PLAYLIST'],
-        "\\": [spotify.remove_current_from_playlist, 'PLAYLIST'],
-        "]": [spotify.add_current_to_playlist, 'PLAYLIST'],
-        ".": [spotify.remove_current_from_playlist, 'PLAYLIST'],
-        "'": [spotify.add_current_to_playlist, 'PLAYLIST'],
+	k = key.char if hasattr(key,'char') else key.name if hasattr(key,'name') else key
+	
+	if k == SHORTCUTS['pause']:
+			pause = not pause
+			if not pause:
+					print(f"RESUMED\n{line()}")
 
-        "f2": [spotify.add_current_to_liked, None],
-        "f3": [spotify.remove_current_from_liked, None],
-        # "<": [spotify.loop_toggle, None],
-        # "M": [spotify.shuffle_toggle, None],
-        # don't change 'pause'
-        "pause": "f4"
-    }
+	if pause and (k in list(SHORTCUTS.keys()) or k == SHORTCUTS[PAUSE]):
+			print(f"PAUSED [{SHORTCUTS['pause']} for resume]\n{line()}")
+			return
+	if pause:
+			return
 
-    try:
-        k = key.char
-    except:
-        try:
-            k = key.name
-        except:
-            k = key
-    if k == shorcuts['pause']:
-        pause = not pause
-        if not pause:
-            print(f"RESUMED\n{spotify.line()}")
+	if k not in SHORTCUTS:
+			return
 
-    if pause and (k in list(shorcuts.keys()) or k == shorcuts['pause']):
-        print(f"PAUSED [{shorcuts['pause']} for resume]\n{spotify.line()}")
-        return
-
-    if pause:
-        return
-
-    if k not in shorcuts:
-        return
-
-    if shorcuts[k][1]:
-        shorcuts[k][0](shorcuts[k][1])
-    else:
-        shorcuts[k][0]()
+	if SHORTCUTS[k][1]:
+			SHORTCUTS[k][0](SHORTCUTS[k][1])
+	else:
+			SHORTCUTS[k][0]()
 
 
 def close():
-    print(f"BYE!\n{spotify.line()}")
-    spotify.exit()
-    sleep(2)
-    exit()
+	print(f"BYE!\n{SPOTIFY.line()}")
+	SPOTIFY.exit()
+	sleep(2)
+	exit()
 
 
-spotify = s.Spotify_custom()
 
 
 def listener():
-    with keyboard.Listener(on_press=press) as listener:
-        listener.join()
+	with keyboard.Listener(on_press=press) as listener:
+			listener.join()
 
 
 th1 = threading.Thread(target=listener, name="LISTENER").start()
