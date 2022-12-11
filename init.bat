010cc836f4496c04c13206fef7501908e6c2bd04
Oleksandr <sandoramixer61@gmail.com>
2022-12-11 03:06:03 +0100

    updated

--- init.bat
+++ init.bat
@@ -1,2 +1,2 @@
 py -m venv python-env
-python-env\Scripts\activate & pip install -r "requirements.txt" & echo "cls ^& python-env\Scripts\activate ^& python main.py" > start.bat
+python-env\Scripts\activate & pip install -r "requirements.txt" & echo cls ^& python-env\Scripts\activate ^& python main.py > start.bat
